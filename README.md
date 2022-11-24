abd45ca4537b1a438c8ad4c59301e407e0422b2c
vidya <vzg805@gmail.com>
2022-11-24 01:21:33 -0600

    added subtle shading

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 <img width="800" alt="Screenshot 2022-11-24 at 12 15 42 AM" src="https://user-images.githubusercontent.com/28833281/203708053-16e49437-cb0c-4a20-82e9-bb427d9e77c7.png">
 
-
 - to run local: `yarn start`
 - to build: `yarn build`
 - to deploy: `yarn run deploy`
@@ -16,9 +15,10 @@ to add (maybe):
 
 - `x` to delete notes
 - new note with `+`
-- shading
 - resize
 - clicking brings to top
 - componentize
 - color change
-
+- typeable/editable
+- cache changes to local storage
+- socket updates
--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import Draggable from "react-draggable";
 const YellowSticky = styled.div`
   word-wrap: break-word;
   position: absolute;
-  background-color: LemonChiffon;
+  background-color: rgba(232, 224, 149);
   padding: 0.5em 1em;
+  border-top: 2rem solid rgba(242, 237, 191);
   width: 15em;
-  height: 15em;
+  height: 13em;
   margin: 0.3em;
   cursor: move;
   text-align: left;
   -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(15deg, rgba(2232, 224, 149) 0%, rgba(250, 246, 212) 100%);
 `;
 
 const PinkSticky = styled.div`
@@ -25,14 +27,16 @@ const PinkSticky = styled.div`
   position: absolute;
   background-color: LightPink;
   padding: 0.5em 1em;
+  border-top: 2rem solid rgb(252, 199, 206);
   width: 15em;
-  height: 15em;
+  height: 13em;
   margin: 0.3em;
   cursor: move;
   text-align: left;
   -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(15deg, LightPink 0%, rgba(255, 212, 217) 100%);
 `;
 
 const PurpleSticky = styled.div`
@@ -40,14 +44,16 @@ const PurpleSticky = styled.div`
   position: absolute;
   background-color: Thistle;
   padding: 0.5em 1em;
+  border-top: 2rem solid rgba(227, 197, 227);
   width: 15em;
-  height: 15em;
+  height: 13em;
   margin: 0.3em;
   cursor: move;
   text-align: left;
   -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(15deg, Thistle 0%, rgba(235, 211, 235) 100%);
 `;
 
 const GreenSticky = styled.div`
@@ -55,14 +61,16 @@ const GreenSticky = styled.div`
   position: absolute;
   background-color: LightGreen;
   padding: 0.5em 1em;
+  border-top: 2rem solid rgba(170, 240, 170);
   width: 15em;
-  height: 15em;
+  height: 13em;
   margin: 0.3em;
   cursor: move;
   text-align: left;
   -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(15deg, LightGreen 0%, rgba(187, 250, 187) 100%);
 `;
 
 const BlueSticky = styled.div`
@@ -70,14 +78,16 @@ const BlueSticky = styled.div`
   position: absolute;
   background-color: LightBlue;
   padding: 0.5em 1em;
+  border-top: 2rem solid rgba(185, 225, 237);
   width: 15em;
-  height: 15em;
+  height: 13em;
   margin: 0.3em;
   cursor: move;
   text-align: left;
   -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(15deg, LightBlue 0%, rgba(200, 236, 247) 100%);
 `;
 
 function App() {
@@ -136,12 +146,8 @@ function App() {
         <div ref={nodeRef}>
           <GreenSticky id={"flooding"}>
             {
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.houstonpublicmedia.org/articles/shows/below-the-waterlines/2022/08/16/430535/flood-control-goes-green-how-houston-is-using-nature-to-combat-flooding/"
-              >
-                https://www.houstonpublicmedia.org/articles/shows/below-the-waterlines/2022/08/16/430535/flood-control-goes-green-how-houston-is-using-nature-to-combat-flooding/
+              <a target="_blank" rel="noreferrer" href="https://mushroomcoloratlas.com/">
+                https://mushroomcoloratlas.com/
               </a>
             }
           </GreenSticky>
