31d4851484f805934e21cf096083fa9897c988cb
vidya <vzg805@gmail.com>
2022-11-24 00:14:21 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+sticky links experiment
+
+- to run local: `yarn start`
+- to build: `yarn build`
+- to deploy: `yarn run deploy`
+
+packages:
+
+- [react-draggable](https://github.com/react-grid-layout/react-draggable/)
+- [styled-components](https://styled-components.com/)
+
+to add (maybe):
+
+- x
+- -
+- shading
+- resize
+- clicking brings to top
+- componentize
+- color change
---
+++ src/App.js
@@ -0,0 +1,207 @@
+import "./App.css";
+import "./fonts/segoepr.ttf";
+
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+import Draggable from "react-draggable";
+
+const YellowSticky = styled.div`
+  word-wrap: break-word;
+  position: absolute;
+  background-color: LemonChiffon;
+  padding: 0.5em 1em;
+  width: 15em;
+  height: 15em;
+  margin: 0.3em;
+  cursor: move;
+  text-align: left;
+  -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const PinkSticky = styled.div`
+  word-wrap: break-word;
+  position: absolute;
+  background-color: LightPink;
+  padding: 0.5em 1em;
+  width: 15em;
+  height: 15em;
+  margin: 0.3em;
+  cursor: move;
+  text-align: left;
+  -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const PurpleSticky = styled.div`
+  word-wrap: break-word;
+  position: absolute;
+  background-color: Thistle;
+  padding: 0.5em 1em;
+  width: 15em;
+  height: 15em;
+  margin: 0.3em;
+  cursor: move;
+  text-align: left;
+  -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const GreenSticky = styled.div`
+  word-wrap: break-word;
+  position: absolute;
+  background-color: LightGreen;
+  padding: 0.5em 1em;
+  width: 15em;
+  height: 15em;
+  margin: 0.3em;
+  cursor: move;
+  text-align: left;
+  -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const BlueSticky = styled.div`
+  word-wrap: break-word;
+  position: absolute;
+  background-color: LightBlue;
+  padding: 0.5em 1em;
+  width: 15em;
+  height: 15em;
+  margin: 0.3em;
+  cursor: move;
+  text-align: left;
+  -moz-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+`;
+
+function App() {
+  const [positions, setPositions] = useState({});
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const nodeRef = useRef(null);
+
+  useEffect(() => {
+    const existingDivPositions = JSON.parse(localStorage.getItem("positions_div"));
+    setPositions(existingDivPositions);
+    setHasLoaded(true);
+    console.log(existingDivPositions);
+    console.log("has loaded");
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(`positions_div`, JSON.stringify(positions));
+  }, [positions]);
+
+  return hasLoaded ? (
+    <div className="App" style={{ height: "100vh", width: "30vw" }}>
+      <Draggable defaultPosition={{ x: 20, y: 20 }} key={4} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <BlueSticky id={"d"}>
+            {
+              <a target="_blank" rel="noreferrer" href="https://ravel.neocities.org/">
+                https://ravel.neocities.org/
+              </a>
+            }
+          </BlueSticky>
+        </div>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 120, y: 350 }} key={1} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <YellowSticky id={"noodledesk"}>
+            {
+              <a target="_blank" rel="noreferrer" href="https://noodledesk.net/index.html">
+                https://noodledesk.net/index.html/
+              </a>
+            }
+          </YellowSticky>
+        </div>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 370, y: 150 }} key={2} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <PinkSticky id={"poetic-web"}>
+            {
+              <a target="_blank" rel="noreferrer" href="https://chiaski.github.io/poeticweb/">
+                https://chiaski.github.io/poeticweb/
+              </a>
+            }
+          </PinkSticky>
+        </div>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 650, y: 0 }} key={3} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <GreenSticky id={"flooding"}>
+            {
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.houstonpublicmedia.org/articles/shows/below-the-waterlines/2022/08/16/430535/flood-control-goes-green-how-houston-is-using-nature-to-combat-flooding/"
+              >
+                https://www.houstonpublicmedia.org/articles/shows/below-the-waterlines/2022/08/16/430535/flood-control-goes-green-how-houston-is-using-nature-to-combat-flooding/
+              </a>
+            }
+          </GreenSticky>
+        </div>
+      </Draggable>
+
+      <Draggable defaultPosition={{ x: 450, y: 300 }} key={4} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <PurpleSticky id={"rubberdust"}>
+            {
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://kenyonreview.org/kr-online-issue/2019-janfeb/selections/sarah-thankam-mathews-342846/"
+              >
+                https://kenyonreview.org/kr-online-issue/2019-janfeb/selections/sarah-thankam-mathews-342846/
+              </a>
+            }
+          </PurpleSticky>
+        </div>
+      </Draggable>
+
+      <Draggable defaultPosition={{ x: 600, y: 500 }} key={5} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <BlueSticky id={"tomine"}>
+            {
+              <a target="_blank" rel="noreferrer" href="https://adriantomine.substack.com/">
+                https://adriantomine.substack.com/
+              </a>
+            }
+          </BlueSticky>
+        </div>
+      </Draggable>
+
+      <Draggable defaultPosition={{ x: 800, y: 100 }} key={6} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <YellowSticky id={"on-a-sunbean"}>
+            {
+              <a target="_blank" rel="noreferrer" href="https://www.onasunbeam.com/">
+                https://www.onasunbeam.com/
+              </a>
+            }
+          </YellowSticky>
+        </div>
+      </Draggable>
+
+      <Draggable defaultPosition={{ x: 780, y: 200 }} key={7} nodeRef={nodeRef}>
+        <div ref={nodeRef}>
+          <PinkSticky id={"datamoshing"}>
+            {
+              <a target="_blank" rel="noreferrer" href="http://datamoshing.com/">
+                http://datamoshing.com/
+              </a>
+            }
+          </PinkSticky>
+        </div>
+      </Draggable>
+    </div>
+  ) : // </div>
+  null;
+}
+
+export default App;
